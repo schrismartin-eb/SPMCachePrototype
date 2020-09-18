87ce4937ba3ebac0652cc7962275678251dca9df
Chris Martin <chris.martin@eventbrite.com>
2020-09-18 09:34:38 -0500

    Initial Commit

---
+++ SPMCachePrototype/SPMCachePrototypeApp.swift
@@ -0,0 +1,17 @@
+//
+//  SPMCachePrototypeApp.swift
+//  SPMCachePrototype
+//
+//  Created by Chris Martin on 9/18/20.
+//
+
+import SwiftUI
+
+@main
+struct SPMCachePrototypeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
